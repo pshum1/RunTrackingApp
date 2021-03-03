24fd45d37491b0a694ffac2bdb99fc3cadbd8dd1
Pierre Shum <pshum1@hawk.iit.edu>
2021-03-03 15:28:45 -0600

    heylo

---
+++ app/src/main/java/com/example/runningappdaggerhilt/TestClass.kt
@@ -0,0 +1,11 @@
+package com.example.runningappdaggerhilt
+
+class TestClass {
+    fun getString () {
+        print("get String")
+    }
+
+    fun getPring() {
+        print("get Pring")
+    }
+}
